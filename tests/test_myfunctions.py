5713d926ebb13bf83c789dbbf2d028810bd8bedc
nlaibr <ahmedfx@gmail.com>
2023-02-19 16:14:31 +0100

    updated files

--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -1,4 +1,4 @@
 from mypythonlib import myfunctions
 def test_haversine():
-    assert myfunctions.haversine(52.370216, 4.895168, 52.520008, 13.404954) == 946.3876221719836
-    #945793.4375088713
+    assert myfunctions.haversine(52.370216, 4.895168, 52.520008, 13.404954) == 946.3876221719835
+    #946.3876221719836
