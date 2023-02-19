6d94fc13df487f8abbb4083ec1f999c6e5eee480
nlaibr <ahmedfx@gmail.com>
2023-02-19 20:18:07 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     description='My first Python library',
     author='Me',
     license='MIT',
-    install_requires=[],
-    setup_requires=['pytest-runner'],
+    install_requires=['twine'],
+    setup_requires=['pytest-runner', 'twine'],
     tests_require=['pytest==7.2.1'],
     test_suite='tests',
 )
