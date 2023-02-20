9dc5e0fdbb84d54ba598e043411c06a94daaec9f
nlaibr <ahmedfx@gmail.com>
2023-02-20 14:19:01 +0100

    updated files

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 setup(
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.9',
+    version='0.1.10',
     description='My first Python library',
     author='Me',
     license='MIT',
     install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==7.2.1'],
+    setup_requires=['pytest-runner', 'pytest-pylint'],
+    tests_require=['pytest', 'pylint'],
     test_suite='tests',
 )
