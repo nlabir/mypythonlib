ac98875a4c8b5b10b03c89ce33b54387ba56b7a9
nlaibr <ahmedfx@gmail.com>
2023-02-20 14:45:19 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 setup(
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.13',
+    version='0.1.14',
     description='My first Python library',
     author='Me',
     license='MIT',
-    install_requires=['pylint'],
-    setup_requires=['pytest-runner', 'pytest-pylint'],
-    tests_require=['pytest', 'pylint'],
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     test_suite='tests',
 )
