cf86ae5c13cc212b10adb089044cbe200c3549aa
nlaibr <ahmedfx@gmail.com>
2023-02-20 15:06:32 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.20',
+    version='0.1.21',
     description='My first Python library',
     author='Me',
     license='MIT',
