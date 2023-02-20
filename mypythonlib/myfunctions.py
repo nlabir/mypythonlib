1dd9f36061d6dd5e9e237b52e399d8d793897348
nlaibr <ahmedfx@gmail.com>
2023-02-20 14:49:27 +0100

    updated libs

--- mypythonlib/myfunctions.py
+++ mypythonlib/myfunctions.py
@@ -17,7 +17,7 @@ def haversine(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
     # Haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
-    r = 6371 # Radius of earth in kilometers
-    return c * r
+    var_a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    var_c = 2 * asin(sqrt(var_a))
+    var_r = 6371 # Radius of earth in kilometers
+    return var_c * var_r
