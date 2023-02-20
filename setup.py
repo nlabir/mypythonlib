110db4fabd266ea858c983bdd14feb3d64389993
nlaibr <ahmedfx@gmail.com>
2023-02-20 14:33:48 +0100

    updated libs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.10',
+    version='0.1.11',
     description='My first Python library',
     author='Me',
     license='MIT',
