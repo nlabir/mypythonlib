c3f0a336c4f20d0fcbf0f1078b851e07f15eb313
nlaibr <ahmedfx@gmail.com>
2023-02-20 11:49:45 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 setup(
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.4',
+    version='0.1.5',
     description='My first Python library',
     author='Me',
     license='MIT',
-    install_requires=[],
-    setup_requires=['pytest-runner'],
+    install_requires=['twine'],
+    setup_requires=['pytest-runner', 'twine'],
     tests_require=['pytest==7.2.1'],
     test_suite='tests',
 )
