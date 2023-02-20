79f5458480a233ffd6278a1d9dae4c03f91b8349
nlaibr <ahmedfx@gmail.com>
2023-02-20 15:03:38 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.19',
+    version='0.1.20',
     description='My first Python library',
     author='Me',
     license='MIT',
